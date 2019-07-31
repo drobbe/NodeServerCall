3047cd9cde563bae4706004cffc050802fa8e572
Edgard <edgard1394@gmail.com>
2019-07-31 18:21:55 -0500

    no push

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var app = require('express')();
 
 var fs = require('fs');
 var options = {
-  key: fs.readFileSync('/etc/apache2/ssl/mibot.key.pem'),
-  cert: fs.readFileSync('/etc/apache2/ssl/1e771d627c3cca1a.crt.pem')
+    key: fs.readFileSync('/etc/apache2/ssl/mibot.key.pem'),
+    cert: fs.readFileSync('/etc/apache2/ssl/1e771d627c3cca1a.crt.pem')
 };
 var https = require('https').createServer(options,app);
 var io = require('socket.io')(https);
@@ -16,24 +16,32 @@ console.log('Configuraciones: '+config.sigma.userDB);
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: config.sigma.userDB,
-  password: config.sigma.passDB
+    host: "localhost",
+    user: config.sigma.userDB,
+    password: config.sigma.passDB
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+    if (err) throw err;
+    console.log("Connected!");
 });
+
 
 io.on('connection', function (socket) {
     socket.on('disconnect', function () {
+        con.query("Update agente set status = 4 where usuario = "+usuario, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+        });
         console.log(socket.usuario + ' se desconecto del chat.' + socket.id);
     });
     
     socket.on('join', function (usuario) {
         socket.usuario = usuario;
-
+        con.query("Update agente set status = 3 where usuario = "+usuario, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+        });
         console.log(socket.usuario + ' se ha conectado.');
     });
 
