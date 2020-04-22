354456699875a6cca86136dbdc71f9da88884562
edgard1394@gmail.com <edgard1394@gmail.com>
2020-04-22 18:11:47 -0500

    test

--- index.js
+++ index.js
@@ -510,6 +510,10 @@ app.get('/usuario/:usuario/reanudar', function(req, res) {
 
 function verficiarUsuarios() {
     Object.keys(clientes).forEach(function(key) {
+        if(clientes[key].tiempo == undefined);
+        {
+            return;
+        }
         clientes[key].tiempo = clientes[key].tiempo + 1;
         console.log(key, clientes[key]);
 
